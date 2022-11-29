8aafc3b7968e4e989239697f0cebaea697fb238b
Andrew Glick <17516195+Antyos@users.noreply.github.com>
2022-11-29 17:50:13 -0600

    Add time-based expand/contract
    Works surprisingly well

--- test_three_nodes.m
+++ test_three_nodes.m
@@ -1,4 +1,5 @@
-set(0,'DefaultFigureWindowStyle','docked')
+clear;
+% set(0,'DefaultFigureWindowStyle','docked')
 rowmag = @(A) sqrt(sum(A.^2,2));  % Magnitude of each row
 bound = @(A, lower, upper) min(max(A,lower),upper);
 edge_len = @(x, i, j) norm(x(j,:)-x(i,:));
@@ -16,12 +17,14 @@ tail = 1;
 head = 3;
 
 edges = [1 2; 2 3];
-weights = [-1 1];
+weights = [1 1];
+isExpanding = logical([1 0 0]);
 
 G = graph(edges(:,1), edges(:,2), weights);
 
 % Mode 1: Edge (1,2) expands, others contract 
 % Mode 3: Edge (2,3) expands, others contract
+
 mode = 1;
 
 tolerance = 0.1;
@@ -30,10 +33,7 @@ f = figure(1);
 clf(f);
 debug_box = annotation( ...
     textbox=[0.2, 0.5, 0.3, 0.3],...
-    String=sprintf( ...
-        "Mode=%d\nE(1,2)=%.3f\nE(2,3)=%.3f", ...
-        mode, edge_len(x, 1, 2), edge_len(x,2,3) ...
-    ), ...
+    String="", ...
     FitBoxToText="on" ...
 );
 for t = 1:1000
@@ -43,14 +43,16 @@ for t = 1:1000
     % Consensus
     for i = 1:N
         % Skip leader; it's static
-        if i == mode
+        if isExpanding(i)
             continue
         end
 
         % Neighbors
         for j = neighbors(G, i)'  % Transpose because Matlab is dumb
             w = G.Edges.Weight(findedge(G, i, j));
-            if mode == 1
+            % If either node is expanding, use max_dist as target
+            % If both nodes are contracting, use min_dist as target
+            if mode == 1 % any(isExpanding([i, j]))
                 target_dist = max_dist;
             else
                 target_dist = min_dist;
@@ -63,25 +65,21 @@ for t = 1:1000
     % Mode swapping. The state of the system is fully controlled by E(1,2)
 
     % Done expanding edge   (1,2)
-    if mode == 1 && (edge_len(x, 1, 2) >= max_dist - tolerance)
-        mode = 3;
-        G.Edges.Weight(:) = [1 -1];
-    end
-    % Done contracting edge (1,2)
-    if mode == 3 && (edge_len(x, 1, 2) <= min_dist + tolerance)
-        mode = 1;
-        G.Edges.Weight(:) = [-1 1];
-    end
-%     % Done contracting edge (2,3)
-%     if mode == 1 && (edge_len(x, 2, 3) <= min_dist + tolerance)
-%         mode = 3;
-%         G.Edges.Weight(:) = [-1 1];
+%     if mode == 1 && (edge_len(x, 1, 2) >= max_dist - tolerance)
+%         mode = 0;
+%         isExpanding([1,3]) = ~isExpanding([1,3]);
+% %         G.Edges.Weight(1) = 1;
 %     end
-%     % Done expanding edge   (2,3)
-%     if mode == 3 && (edge_len(x, 2, 3) >= max_dist - tolerance)
+%     % Done contracting edge (1,2)
+%     if mode == 0 && (edge_len(x, 1, 2) <= min_dist + tolerance)
 %         mode = 1;
-%         G.Edges.Weight(:) = [-1 1];
+%         isExpanding([1,3]) = ~isExpanding([1,3]);
+% %         G.Edges.Weight(1) = -1;
 %     end
+    if mod(t, 50) == 0
+        mode = ~mode;
+        isExpanding([1,3]) = ~isExpanding([1,3]);
+    end
 
     % Limit velocity to dx_max
     constrained_dx = dx./rowmag(dx).*min(dx_max, rowmag(dx));
@@ -93,10 +91,13 @@ for t = 1:1000
     x(:,2) = bound(x(:,2), 0, 6);
 
     set(debug_box, String=sprintf( ...
-        "Mode=%d\nE(1,2)=%.3f\nE(2,3)=%.3f", ...
-        mode, edge_len(x, 1, 2), edge_len(x,2,3) ...
+        "Mode=%d\nE(1,2)=%.3f (%d,w=%d)\nE(2,3)=%.3f (%d,w=%d)", ...
+        mode,...
+        edge_len(x,1,2), isExpanding(1), G.Edges.Weight(1), ...
+        edge_len(x,2,3), isExpanding(3), G.Edges.Weight(2) ...
     ));
-    plot(G, XData=x(:,1), YData=x(:,2));
+    p = plot(G, XData=x(:,1), YData=x(:,2));
+    highlight(p, isExpanding, NodeColor="red")
 %     hold on
     % Velocity vectors for convenience
 %     quiver(x(:,1), x(:,2), dx(:,1), dx(:,2), 0.3);
