ccbd9e1a7779b976b512a6611644fc0b97106326
Andrew Glick <17516195+Antyos@users.noreply.github.com>
2022-11-29 11:35:31 -0600

    Clean up edge lengths, add annotations, clearer modes

--- test_three_nodes.m
+++ test_three_nodes.m
@@ -1,31 +1,53 @@
 set(0,'DefaultFigureWindowStyle','docked')
 rowmag = @(A) sqrt(sum(A.^2,2));  % Magnitude of each row
 bound = @(A, lower, upper) min(max(A,lower),upper);
+edge_len = @(x, i, j) norm(x(j,:)-x(i,:));
 
-tailpush = true;
-
-x = [1 2; 2 2];
 
 min_dist = 0.5;
-max_dist = 1;
-
-dx = zeros(size(x));
+max_dist = 1.5;
 dx_max = 0.02;
 
+x = [1 2; 2 2; 3 2];
+dx = zeros(size(x));
 N = length(x);
 
-edges = [1 2];
-weights = [1];
+tail = 1;
+head = 3;
+
+edges = [1 2; 2 3];
+weights = [-1 1];
 
 G = graph(edges(:,1), edges(:,2), weights);
 
-mode = 1; % 1:Push from tail(1), -1:Pull to head(5)
-clearance = 0.1;
+% Mode 1: Edge (1,2) expands, others contract 
+% Mode 3: Edge (2,3) expands, others contract
+mode = 1;
 
+tolerance = 0.1;
+
+f = figure(1);
+clf(f);
+debug_box = annotation( ...
+    textbox=[0.2, 0.5, 0.3, 0.3],...
+    String=sprintf( ...
+        "Mode=%d\nE(1,2)=%.3f\nE(2,3)=%.3f", ...
+        mode, edge_len(x, 1, 2), edge_len(x,2,3) ...
+    ), ...
+    FitBoxToText="on" ...
+);
 for t = 1:1000
-    dx(:,:) = 0; % Reset dx
+    % Reset dx
+    dx(:,:) = 0;
+
     % Consensus
     for i = 1:N
+        % Skip leader; it's static
+        if i == mode
+            continue
+        end
+
+        % Neighbors
         for j = neighbors(G, i)'  % Transpose because Matlab is dumb
             w = G.Edges.Weight(findedge(G, i, j));
             if mode == 1
@@ -33,20 +55,33 @@ for t = 1:1000
             else
                 target_dist = min_dist;
             end
-            dx(i,:) = dx(i,:) + 0.05*abs(w)*(norm(x(j,:)-x(i,:))^2 - target_dist^2)*(x(j,:)-x(i,:));
+
+            dx(i,:) = dx(i,:) + 0.1*abs(w)*(edge_len(x, i, j)^2 - target_dist^2)*(x(j,:)-x(i,:));
         end
     end
 
     % Mode swapping
-    if mode == 1 && (norm(x(1,:)-x(2,:)) >= max_dist - clearance)
-        mode = 2;
-    end
-    if mode == 2 && (norm(x(1,:)-x(2,:)) <= min_dist + clearance)
-        mode = 1;
-    end
 
-    % Leader is static here
-    dx(mode,:) = 0;
+    % Done expanding edge   (1,2)
+    if mode == 1 && (edge_len(x, 1, 2) >= max_dist - tolerance)
+        mode = 3;
+        G.Edges.Weight(:) = [1 -1];
+    end
+    % Done contracting edge (1,2)
+    if mode == 3 && (edge_len(x, 1, 2) <= min_dist + tolerance)
+        mode = 1;
+        G.Edges.Weight(:) = [-1 1];
+    end
+    % Done contracting edge (2,3)
+    if mode == 1 && (edge_len(x, 2, 3) <= min_dist + tolerance)
+        mode = 3;
+        G.Edges.Weight(:) = [1 -1];
+    end
+    % Done expanding edge   (2,3)
+    if mode == 3 && (edge_len(x, 2, 3) >= max_dist - tolerance)
+        mode = 1;
+        G.Edges.Weight(:) = [-1 1];
+    end
 
     % Limit velocity to dx_max
     constrained_dx = dx./rowmag(dx).*min(dx_max, rowmag(dx));
@@ -56,11 +91,15 @@ for t = 1:1000
     % Constrain to screen
     x(:,1) = bound(x(:,1), 0, 6);
     x(:,2) = bound(x(:,2), 0, 6);
-    %hold on
+
+    set(debug_box, String=sprintf( ...
+        "Mode=%d\nE(1,2)=%.3f\nE(2,3)=%.3f", ...
+        mode, edge_len(x, 1, 2), edge_len(x,2,3) ...
+    ));
     plot(G, XData=x(:,1), YData=x(:,2));
-    hold on
+%     hold on
     % Velocity vectors for convenience
-    quiver(x(:,1), x(:,2), dx(:,1), dx(:,2), 0.3);
+%     quiver(x(:,1), x(:,2), dx(:,1), dx(:,2), 0.3);
     xlim([0 7])
     ylim([0 5])
     hold off
