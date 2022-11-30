cb65dcc48c69f4e0151756b7780bc25467d33b7e
Andrew Glick <17516195+Antyos@users.noreply.github.com>
2022-11-29 18:45:31 -0600

    Improve plotting

--- test_three_nodes.m
+++ test_three_nodes.m
@@ -4,10 +4,12 @@ rowmag = @(A) sqrt(sum(A.^2,2));  % Magnitude of each row
 bound = @(A, lower, upper) min(max(A,lower),upper);
 edge_len = @(x, i, j) norm(x(j,:)-x(i,:));
 
+window = [0 0 6 6]; % (x, y, w, h)
+border = 0.5;
 
 tolerance = 0.1;
 
-min_dist = 0.4;
+min_dist = 0.3;
 max_dist = 1.5;
 dx_max = 0.02;
 
@@ -26,14 +28,22 @@ isExpanding = 1;
 
 G = graph(edges(:,1), edges(:,2), weights);
 
-% Plotting
+% Setup plotting
 f = figure(1);
 clf(f);
+p = plot(G, XData=x(:,1), YData=x(:,2));
 debug_box = annotation( ...
     textbox=[0.2, 0.5, 0.3, 0.3],...
     String="", ...
     FitBoxToText="on" ...
 );
+rectangle(Position=window);
+xlim([-border, window(3)+border]+window(1));
+ylim([-border, window(4)+border]+window(2));
+axis equal
+
+
+% Run simulation
 for t = 1:1000
     % Reset dx
     dx(:,:) = 0;
@@ -81,28 +91,28 @@ for t = 1:1000
     x = x + constrained_dx;
 
     % Constrain to screen
-    x(:,1) = bound(x(:,1), 0, 6);
-    x(:,2) = bound(x(:,2), 0, 6);
+    x(:,1) = bound(x(:,1), window(1), window(1)+window(3));
+    x(:,2) = bound(x(:,2), window(2), window(2)+window(4));
 
     if any(x(:)==6) || any(x(:)==0)
         isFixed([1,end]) = ~isFixed([1,end]);
     end
 
-    set(debug_box, String=sprintf( ...
+    % Update plot
+    debug_box.String=sprintf( ...
         "Mode=%d\nE(1,2)=%.3f (%d,w=%d)\nE(2,3)=%.3f (%d,w=%d)", ...
         isExpanding,...
         edge_len(x,1,2), isFixed(1), G.Edges.Weight(1), ...
         edge_len(x,2,3), isFixed(3), G.Edges.Weight(2) ...
-    ));
-    p = plot(G, XData=x(:,1), YData=x(:,2));
+    );
+    p.XData = x(:, 1);
+    p.YData = x(:, 2);
+
+    % Update node colors
+    p.NodeColor = "#0072BD";  % Default blue
     highlight(p, isFixed, NodeColor="red")
-%     hold on
     % Velocity vectors for convenience
-%     quiver(x(:,1), x(:,2), dx(:,1), dx(:,2), 0.3);
-    xlim([0 7])
-    ylim([0 5])
-    hold off
-    axis equal
+    % quiver(x(:,1), x(:,2), dx(:,1), dx(:,2), 0.3);
 
     pause(0.05)
 end
