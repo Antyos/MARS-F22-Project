cb1b49affae25a51ddbe4b9cf563557795ad9485
Andrew Glick <17516195+Antyos@users.noreply.github.com>
2022-11-30 17:11:47 -0600

    Use delta-disk model for edges

--- test_five_nodes.m
+++ test_five_nodes.m
@@ -3,17 +3,20 @@ clear;
 rowmag = @(A) sqrt(sum(A.^2,2));  % Magnitude of each row
 bound = @(A, lower, upper) min(max(A,lower),upper);
 edge_len = @(x, i, j) norm(x(j,:)-x(i,:));
+fi = @(varargin)varargin{length(varargin)-varargin{1}};
 
 window = [0 0 6 6]; % (x, y, w, h)
 border = 0.5;
+iterations = 2000;
 
-tolerance = 0.1;
+close_enough = 0.1;
 
 min_dist = 0.6;
 max_dist = 1.5;
-dx_max = 0.02;
+dx_max = 0.04;
+dx_gain = 0.2;
 
-x = [1 2; 2 2; 3 3; 3 1; 4 2];  % Position
+x = [1 2; 2 2; 3 3; 3 1; 4 2; 4 3];  % Position
 dx = zeros(size(x)); % Velocity
 N = length(x);  % Num nodes
 
@@ -22,21 +25,23 @@ isLeader([1, end]) = true;
 isFixed = false(N, 1);  % Bool mask of which nodes are fixed
 isFixed(1) = true;
 
+edges = proximity_graph(x, max_dist);
+weights = ones(height(edges), 1);
 
-edges = [1 2; 2 3; 3 4; 2 4; 1 3; 3 5; 4 5];
-weights = ones(length(edges), 1);
+isExpanding = true;
+super_leader = 1;  % Node responsible for controlling expansion
 
-isExpanding = 1;
-
-G = graph(edges(:,1), edges(:,2), weights);
+G = graph(edges(:,1), edges(:,2), weights, N);
 
 % Setup plotting
 f = figure(1);
 clf(f);
-p = plot(G, XData=x(:,1), YData=x(:,2));
+p = plot(G, XData=x(:,1), YData=x(:,2), ...
+    EdgeColor="black" ...
+);
 hold on
 % Velocity vectors for convenience
-q = quiver(x(:,1), x(:,2), dx(:,1), dx(:,2), 0.3);
+% q = quiver(x(:,1), x(:,2), dx(:,1), dx(:,2), 0.5);
 hold off
 debug_box = annotation( ...
     textbox=[0.2, 0.6, 0.3, 0.3],...
@@ -56,9 +61,22 @@ for t = 1:1000
     % Reset dx
     dx(:,:) = 0;
 
+    % Update graph
+    new_edges = proximity_graph(x, max_dist/2, G);
+    if ~isempty(new_edges)
+        G = addedge(G, new_edges(:,1), new_edges(:,2), length(new_edges));
+        % Replot
+        p = plot(G, XData=x(:,1), YData=x(:,2), ...
+            EdgeColor="black" ...
+        );
+%         rectangle(Position=window);
+        xlim([-border, window(3)+border]+window(1));
+        ylim([-border, window(4)+border]+window(2));
+    end
+
     % Consensus
     for i = 1:N
-        % Skip leader; it's static
+        % Skip fixed nodes
         if isFixed(i)
             continue
         end
@@ -80,22 +98,22 @@ for t = 1:1000
                 target_dist = min_dist;
             end
 
-            dx(i,:) = dx(i,:) + 0.1*abs(w)*(edge_len(x, i, j)^2 - target_dist^2)*(x(j,:)-x(i,:));
+            dx(i,:) = dx(i,:) + dx_gain*abs(w)*(edge_len(x, i, j)^2 - target_dist^2)*(x(j,:)-x(i,:));
         end
     end
 
-    %%%%%%%%
-    % Mode swapping. The state of the system is fully controlled by E(1,2)
-    %%%%%%%%
 
-    % Done expanding edge   (1,2)
-    if isExpanding && (edge_len(x, 1, 2) >= max_dist - tolerance)
+    % Mode swapping. The state of the system is fully controlled by E(1,2)
+
+    % Done expanding
+    super_leader_edge_lens = arrayfun(@(j) edge_len(x, super_leader, j), neighbors(G, super_leader));
+    if isExpanding && (max(super_leader_edge_lens) >= max_dist - close_enough)
         isExpanding = false;
         isFixed(isLeader) = ~isFixed(isLeader);
     end
 
-    % Done contracting edge (1,2)
-    if ~isExpanding && (edge_len(x, 1, 2) <= min_dist + tolerance)
+    % Done contracting
+    if ~isExpanding && (min(super_leader_edge_lens) <= min_dist + close_enough)
         isExpanding = true;
         isFixed(isLeader) = ~isFixed(isLeader);
     end
@@ -118,26 +136,48 @@ for t = 1:1000
     end
 
     % Update plot
-    debug_box.String=sprintf("Mode=%d", isExpanding);
+    debug_box.String=strcat( ...
+        "Mode = ", ...
+        fi(isExpanding, "Expanding", "Contracting") ...
+    );
     p.XData = x(:, 1);
     p.YData = x(:, 2);
-    q.XData = x(:, 1);
-    q.YData = x(:, 2);
-    q.UData = dx(:, 1);
-    q.VData = dx(:, 2);
+    %     q.XData = x(:, 1);
+    %     q.YData = x(:, 2);
+    %     q.UData = dx(:, 1);
+    %     q.VData = dx(:, 2);
 
     % Update node colors
-    p.NodeColor = "#0072BD";  % Default blue
+    p.NodeColor = "black";  % Default blue
     highlight(p, isLeader, NodeColor="blue")
     highlight(p, isFixed, NodeColor="red")
     for id = find(isLeader)'
-        if isExpanding
-            c = "green";
-        else
-            c = "magenta";
-        end
-        highlight(p, id, neighbors(G,id), EdgeColor=c);
+        highlight(p, id, neighbors(G,id), EdgeColor=fi(isExpanding,'g', 'm'));
     end
 
     pause(0.05)
+end
+
+%% Helper Functions
+
+% Delta-disk graph: get edges of all nodes within certain distance
+function edges = proximity_graph(x, dist, G)
+    edge_len = @(x, i, j) norm(x(j,1:2)-x(i,1:2));
+
+    N = length(x);
+    edges = cell(0, 1);
+    for i = 1:N-1
+        for j = i+1:N
+            % Skip if we already have an edge
+            if exist('G', 'var') && findedge(G, i, j)
+                continue
+            end
+            
+            % Check distance for edge
+            if edge_len(x, i, j) <= dist
+                edges{end+1,1} = [i j]; %#ok
+            end
+        end
+    end
+    edges = cell2mat(edges);
 end
